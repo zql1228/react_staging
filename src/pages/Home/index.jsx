09a6f786a5c1b6c45188a31f1a9480848048485e
wangliukai <2954535934@qq.com>
2022-07-22 18:17:32 +0800

    react学习第六天 多级路由，路由匹配

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,9 +8,14 @@ export default class Home extends Component {
     return (
       <div>
       <h3>Home组件的内容</h3>
-
-       <MyNavLink to="/home/news">News</MyNavLink>
-       <MyNavLink to="/home/message">Message</MyNavLink>
+      <ul className="nav nav-tabs">
+      <li>
+        <MyNavLink to="/home/news">News</MyNavLink>
+      </li>
+      <li>
+        <MyNavLink to="/home/message">Message</MyNavLink>
+      </li>
+    </ul>
     {/* 注册路由 */}
     <Switch>
       <Route path="/home/news" component={News}></Route>
